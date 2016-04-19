b14092d78efe953d4d66f169697365e9f0dbd0f2
cristiven <cristiven@utp.edu.co>
2016-04-18 23:13:15 -0500

    Modifiction to assignetion of Carry

--- Processor Unicycle 3/PSR.vhd
+++ Processor Unicycle 3/PSR.vhd
@@ -24,24 +24,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity PSR is
 	
 	  Port(clk : in  STD_LOGIC;
-			 reset: in STD_LOGIC;
-			 NZVC : in  STD_LOGIC_VECTOR (3 downto 0);
-          Carry : out  STD_LOGIC);
+	       reset: in STD_LOGIC;
+	       NZVC : in  STD_LOGIC_VECTOR (3 downto 0);
+               Carry : out  STD_LOGIC);
 end PSR;
 
 architecture arq_PSR of PSR is
-
-signal PSRegister: STD_LOGIC_VECTOR (31 DOWNTO 0):= (others=>'0');
 
 begin
 	process(clk,reset)
 	begin
 		if(reset = '1') then
-				Carry <= '0';
+			Carry <= '0';
 		else
 			if(rising_edge(clk))then
-				PSRegister(23 downto 20) <= NZVC;
-				Carry <= PSRegister(20);
+				Carry <= NZVC(0);
 				
 			end if;
 		end if;
