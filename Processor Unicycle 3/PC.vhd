a3ef35d20c8437df44e71180aa949be57cd3997f
cristiven <cristiven@utp.edu.co>
2016-04-22 08:08:37 -0500

    Module PC, without changes

---
+++ Processor Unicycle 3/PC.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity PC is
+    Port ( address : in  STD_LOGIC_VECTOR (31 downto 0);
+           clk : in  STD_LOGIC;
+			  reset : in  STD_LOGIC;
+           out_Instruction : out  STD_LOGIC_VECTOR (31 downto 0));
+end PC;
+ 
+architecture arq_PC of PC is
+
+begin
+	process(clk)
+	begin
+		if(reset = '1')then
+			out_Instruction <= (others=> '0');
+		else
+			if(rising_edge(clk))then
+				out_Instruction <= address;
+			end if;
+		end if;
+end process;	
+
+end arq_PC;
